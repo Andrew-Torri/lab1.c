35f5aa04511fb3b808fc9bac015b7364b8a83da4
TorriAndrew <abt5506@psu.edu>
2020-09-01 18:16:29 +0000

    fixed readline

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
-#include <stdio.h>
+//Author: Andrew Torri abt5506@psu.edu
+//Contributor: Tristan Zanowic tsz5030@psu.edu
+//Contributor: Purushottam Shukla pps5338@psu.edu
+//Contributor: Reem Abdou rpa5285@psu.edu
+#include<stdio.h>
+#include<stdlib.h>
 #include<readline/readline.h>
 
 int main(void) 
@@ -6,6 +11,6 @@ int main(void)
   char *temp = readline("Enter temperature in celsius: ");
   double ctemp = atof(temp);
   double ftemp = (ctemp * 1.8) + 32;
-  printf("%fd° in Celsius is equivalent to %fd° Fahrenheit.", ctemp, ftemp);
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit.", ctemp, ftemp);
   return 0;
 }
